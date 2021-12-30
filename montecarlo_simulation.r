cc8c3b9be4c9c7b48f76e3695e9dc26f30f5778d
VeraYOsa <65073642+VeraYOsa@users.noreply.github.com>
2021-12-30 16:05:03 +0100

    Update montecarlo_simulation.r

--- montecarlo_simulation.r
+++ montecarlo_simulation.r
@@ -11,3 +11,20 @@ results <- replicate(B, {    # returns vector of B logical values
   any(duplicated(bdays))
 })
 mean(results)    # calculates proportion of groups with duplicated bdays
+
+
+# How many montecarlo simulations are needed
+# Code: Estimating a practical value of B
+# This code runs Monte Carlo simulations to estimate the probability of shared birthdays using several B values and plots the results. When B is large enough that the estimated probability stays stable, then we have selected a useful value of B.
+
+B <- 10^seq(1, 5, len = 100)    # defines vector of many B values
+compute_prob <- function(B, n = 22){    # function to run Monte Carlo simulation with each B
+  same_day <- replicate(B, {
+    bdays <- sample(1:365, n, replace = TRUE)
+    any(duplicated(bdays))
+  })
+  mean(same_day)
+}
+
+prob <- sapply(B, compute_prob)    # apply compute_prob to many values of B
+plot(log10(B), prob, type = "l")    # plot a line graph of estimates 
